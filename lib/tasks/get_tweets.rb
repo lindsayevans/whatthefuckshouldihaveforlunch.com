2ccd5179df2de84581187dfb7c19c1c58c0cbcbe
Lindsay Evans <linz@linz.id.au>
2009-10-23 06:10:30 +1100

    Removing Linguistics stuff for now

--- lib/tasks/get_tweets.rb
+++ lib/tasks/get_tweets.rb
@@ -6,13 +6,6 @@ namespace :wtf do
 
 		# Dependencies
 		require 'grackle'
-		require 'linguistics'
-
-		# Brett: What?
-		# Jules: "What" ain't no country I ever heard of! They speak English in "What"?!
-		# Brett: What?
-		# Jules: English, motherfucker! Do you speak it?!
-		Linguistics::use(:en)
 
 		# Models
 		require 'lib/Meal'
@@ -20,26 +13,25 @@ namespace :wtf do
 		from_username = 'lindsayevans'
 
 		hashtag = '#lunch'
-		hashtag_regexp = Regexp.new('[\s]*' + hashtag, true)
+		cleanup_regexp = Regexp.new('[\s]*' + hashtag, true)
 
 		client = Grackle::Client.new
 		client.api = :search
 
 		latest_meal = Meal.find_latest
+		latest_meal = nil
 
-		if !latest_meal.nil?
-			since_id = latest_meal[:twitter_status_id]
-		end
+		since_id = latest_meal.nil? ? nil : latest_meal[:twitter_status_id]
 
 		results = client.search?(:q => 'from:' + from_username + ' ' + hashtag, :since_id => since_id).results
+puts 'from:' + from_username + ' ' + hashtag
 		results.each do |result|
 
-			message = result.text.gsub(hashtag_regexp, '')
-			food = message.en.sentence.object || message.en.sentence.subject || message
-			tweet_attributes = {:food => food.to_s, :message => message.to_s, :twitter_status_id => result.id, :created_at => result.created_at}
+			message = result.text.gsub(cleanup_regexp, '')
+			tweet_attributes = {:message => message.to_s, :twitter_status_id => result.id, :created_at => result.created_at}
 
-			#puts YAML::dump(tweet_attributes)
-			meal = Meal.create tweet_attributes
+			puts YAML::dump(tweet_attributes)
+			#meal = Meal.create tweet_attributes
 		end
 
 	end
