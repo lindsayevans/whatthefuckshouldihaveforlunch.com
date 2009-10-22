d31098ee601f83ba72e9fd0f52c39023fb7bd351
Lindsay Evans <linz@linz.id.au>
2009-10-23 06:17:42 +1100

    Tidying, refactoring

--- lib/tasks/get_tweets.rb
+++ lib/tasks/get_tweets.rb
@@ -19,19 +19,15 @@ namespace :wtf do
 		client.api = :search
 
 		latest_meal = Meal.find_latest
-		latest_meal = nil
 
 		since_id = latest_meal.nil? ? nil : latest_meal[:twitter_status_id]
 
 		results = client.search?(:q => search_query, :since_id => since_id).results
-puts 'from:' + from_username + ' ' + hashtag
 		results.each do |result|
-
 			message = result.text.gsub(cleanup_regexp, '')
 			tweet_attributes = {:message => message.to_s, :twitter_status_id => result.id, :created_at => result.created_at}
-
-			puts YAML::dump(tweet_attributes)
-			#meal = Meal.create tweet_attributes
+			#puts YAML::dump(tweet_attributes)
+			meal = Meal.create tweet_attributes
 		end
 
 	end
