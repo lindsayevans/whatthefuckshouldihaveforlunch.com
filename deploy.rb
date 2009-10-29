5de32ba854d9843135eb28a27db439d166409f79
Lindsay Evans <linz@linz.id.au>
2009-10-29 20:08:47 +1100

    Adding cap deploy script

---
+++ deploy.rb
@@ -0,0 +1,39 @@
+
+#========================
+#CONFIG
+#========================
+set :application, "whatthefuckshouldihaveforlunch.com"
+set :scm, :git
+set :git_enable_submodules, 1
+set :repository, "git://github.com/lindsayevans/whatthefuckshouldihaveforlunch.com.git"
+set :branch, "master"
+set :ssh_options, { :forward_agent => true }
+set :stage, :production
+set :user, "deploy"
+set :use_sudo, false
+set :runner, "deploy"
+set :deploy_to, "/var/www/#{application}"
+set :app_server, :passenger
+set :domain, "whatthefuckshouldihaveforlunch.com"
+#========================
+#ROLES
+#========================
+role :app, domain
+role :web, domain
+role :db, domain, :primary => true
+#========================
+#CUSTOM
+#========================
+namespace :deploy do
+task :start, :roles => :app do
+run "touch #{current_release}/tmp/restart.txt"
+end
+task :stop, :roles => :app do
+# Do nothing.
+end
+desc "Restart Application"
+task :restart, :roles => :app do
+run "touch #{current_release}/tmp/restart.txt"
+end
+end
+
