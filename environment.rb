72e15440853eb4172105cf7543f5d939bcac1fbc
Lindsay Evans <linz@linz.id.au>
2009-12-13 10:28:24 +1100

    fixing DB path in environment

--- environment.rb
+++ environment.rb
@@ -1,5 +1,5 @@
 if ENV['RACK_ENV'] == 'production'
     @@db_path = '/var/www/whatthefuckshouldihaveforlunch.com/shared/db/'
 else
-    @@db_path = '/Users/lindsay/Projects/whatthefuckshouldihaveforlunch.com/db/'
+    @@db_path = '/Users/lindsay/Projects/whatthefuckshouldihaveforlunch.com/shared/db/'
 end
